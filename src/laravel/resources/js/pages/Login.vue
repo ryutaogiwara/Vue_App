36fb274a934641aadd6482274f047de96173fbc9
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-02-21 00:38:58 +0900

    Create TabFunction
    ユーザー登録/ログイン用
    表示切り替えタブ作成

--- src/laravel/resources/js/pages/Login.vue
+++ src/laravel/resources/js/pages/Login.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Login</h1>
+  <div class="container--small">
+    <ul class="tab">
+      <li class="tab__item" @click="tab = 1">
+        Login
+      </li>
+
+      <li class="tab__item" @click="tab = 2">
+        Register
+      </li>
+    </ul>
+    {{ tab }}
+    <div class="panel" v-show="tab === 1">Login Form</div>
+    <div class="panel" v-show="tab === 2">Register Form</div>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 1
+    }
+  }
+}
+</script>
