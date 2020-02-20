9cf8a759ca03fe1455d8810beaee94faa52a126c
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-02-20 17:49:49 +0900

    Update api.php
    Authユーザー登録メソッド呼び出し

--- src/laravel/routes/api.php
+++ src/laravel/routes/api.php
@@ -2,17 +2,9 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
