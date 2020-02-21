8acad14028c09ed70c6a5a58e5c129a07ec13ab1
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-02-21 23:51:47 +0900

    Update Footer.vue
    メソッド追加
    logoutボタンを押した際に
    メソッドが発火。
    
    authAPIにデータが飛んでログアウト処理を実行する

--- src/laravel/resources/js/components/Footer.vue
+++ src/laravel/resources/js/components/Footer.vue
@@ -1,8 +1,21 @@
 <template>
   <footer class="footer">
-    <button class="button button--link">Logout</button>
+    <button class="button button--link" @click="logout">Logout</button>
     <RouterLink class="button button--link" to="/login">
       Login / Register
     </RouterLink>
   </footer>
 </template>
+
+<script>
+export default {
+  methods: {
+    async logout () {
+      // authストアlogoutメソッド呼び出し
+      await this.$store.dispatch('auth/logout')
+      //  logoutメソッド終了後にVuerouterに/loginでアクセス
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
