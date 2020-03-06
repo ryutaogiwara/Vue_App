bea170de88107eb07b850b854b6b572a67dec999
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-06 22:05:45 +0900

    Update index.js
    errorモジュールの組み込み

--- src/laravel/resources/js/store/index.js
+++ src/laravel/resources/js/store/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// ストア読み込み
 import auth from './auth'
+import error from './error'
+
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    auth
+    auth,
+    error
   }
 })
 
