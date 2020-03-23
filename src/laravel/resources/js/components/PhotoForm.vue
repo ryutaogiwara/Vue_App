15d154873c8210c359aa9dee5c3159dc48d6ace6
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-23 16:57:16 +0900

    Update PhotoForm.vue
    親要素側から制御を行うためにprops追加
    制御の内容はvalueに真偽値を与え親要素のボタンがクリックされた際に表示状態をv-showで切り替える

--- src/laravel/resources/js/components/PhotoForm.vue
+++ src/laravel/resources/js/components/PhotoForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="photo-form">
+  <div v-show="value" class="photo-form">
     <h2 class="title">Submit a photo</h2>
     <form class="form">
       <input class="form__item" type="file">
@@ -9,3 +9,18 @@
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  // 親要素(navbar)側で制御を行うためpropsを用いる
+  // propsに送られたvalueを参照して小要素(PhotoForm)の内容が決まる
+  props: {
+    // valueに真偽値を与えることでv-showが機能し、表示/非表示を切り替える
+    value: {
+      type: Boolean,
+      // requiredがないとエラーなどで真偽値情報が飛んだ時に動作しなくなる
+      required: true
+    }
+  }
+}
+</script>
