dbccaf55de862e52dc8a908185a35c0c134cbc4e
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-23 16:17:37 +0900

    Create PhotoForm.vue
    投稿フォーム作成

---
+++ src/laravel/resources/js/components/PhotoForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="photo-form">
+    <h2 class="title">Submit a photo</h2>
+    <form class="form">
+      <input class="form__item" type="file">
+      <div class="form__button">
+        <button type="submit" class="button button--inverse">submit</button>
+      </div>
+    </form>
+  </div>
+</template>
