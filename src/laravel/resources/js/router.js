df181f898f56e17c3cd0714b9d32bb4f59992882
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-23 22:10:38 +0900

    Update router.js
    写真のDetailページ用ルーティング追加

--- src/laravel/resources/js/router.js
+++ src/laravel/resources/js/router.js
@@ -19,6 +19,11 @@ const routes = [
     component: PhotoList
   },
   {
+    path: '/photos/:id',
+    component: PhotoDetail,
+    props: true
+  },
+  {
     path: '/login',
     component: Login,
     beforeEnter(to, from, next) {
