8775c9ca792a6ac2a0136b222cc3f8aedba4250b
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-25 04:23:40 +0900

    Update api.php
    写真一覧ルーティング追加

--- src/laravel/routes/api.php
+++ src/laravel/routes/api.php
@@ -17,6 +17,10 @@ Route::get('/user', fn () => Auth::user())->name('user');
 // 写真投稿
 Route::post('/photos', 'PhotoController@create')->name('photo.create');
 
+// 写真一覧
+Route::get('/photos', 'PhotoController@index')->name('photo.index');
+
+
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
