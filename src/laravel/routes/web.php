a259d602dcdaec0caf9ce1720f870fec053f0dfd
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-28 07:19:11 +0900

    Update web.php
    ダウンロード用のルーティング設定
    このURLはAPIではないのでlaravel
    本体のルーティングに設定する必要あり

--- src/laravel/routes/web.php
+++ src/laravel/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+// 写真ダウンロード
+// インデックスルート依も上におく必要あり('/{any?}'が先に当たるとルートをうまく振り分けられないため)
+Route::get('/photos/{photo}/download', 'PhotoController@download');
+
 // APIのURL以外のリクエストに対してはindexテンプレートを返す
 // 画面遷移はフロントエンドのVueRouterが制御する
 Route::get('/{any?}', fn () => view('index'))->where('any', '.+');
