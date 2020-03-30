68c9a3254cbfce22fd2da327cb4fba855ff786bb
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-30 17:54:15 +0900

    Create Photo.vue
    写真表示のコンポーネント作成

---
+++ src/laravel/resources/js/components/Photo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="photo">
+    <figure class="photo__wrapper">
+      <img 
+        class="photo__image"
+        :src="item.url"
+        :alt="'Photo by ${item.owner.name}'"
+      >
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
