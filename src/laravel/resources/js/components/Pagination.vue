a20ebac25bb337357ed0efa5b3432097eb138198
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-30 22:09:13 +0900

    Create Pagination.vue
    prev,nextボタンの作成

---
+++ src/laravel/resources/js/components/Pagination.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="pagination">
+    <!-- (prev)前へボタンは1ページ目以外の場合に表示 -->
+    <RouterLink
+      v-if="! isFirstPage"
+      :to="`/?page=${currentPage - 1}`"
+      class="button"
+    >&laquo; prev</RouterLink>
+
+    <!-- (next)次へボタンは最終ページ以外のページで表示 -->
+    <RouterLink
+      v-if="! isLastPage"
+      :to="`/?page=${currentPage + 1}`"
+      class="button"
+    >next &raquo;</RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 現在ページ
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    // 最終ページ
+    lastPage: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    // 現在が1ページ目かどうかを判定
+    isFirstPage () {
+      return this.currentPage === 1
+    },
+
+    // 現在が最終ページかどうかを判定
+    isLastPage () {
+      return this.currentPage === this.lastPage
+    }
+  }
+}
+</script>
