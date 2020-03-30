2560cc4b1eb4549f5cca45a13d90c812226b49a4
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-03-30 18:05:47 +0900

    Update Photo.vue
    いいねボタン、ダウンロードボタン、投稿者名のマークアップ実装

--- src/laravel/resources/js/components/Photo.vue
+++ src/laravel/resources/js/components/Photo.vue
@@ -7,13 +7,38 @@
         :alt="'Photo by ${item.owner.name}'"
       >
     </figure>
-    
+
     <!-- オーバーレイ -->
     <RouterLink
       class="photo__overlay"
       :to="`/photos/${item.id}`"
       :title="`View the photo by ${item.owner.name}`"
     >
+      <!-- like button & download link-->
+      <div class="photo__controls">
+        <button
+          class="photo__action photo__action--like"
+          title="Like photo"
+        >
+          <i class="icon ion-md-heart"></i>12
+        </button>
+
+        <!-- ダウンロード昨日はvueのRouterLinkではなくサーバー再度で行うためaタグで用意する必要がある -->
+        <!-- .stopはダウンロードがクリックされた際に親要素のクリックを止めないと画面遷移が起きてしまうため -->
+        <a
+          class="photo__action"
+          title="Download photo"
+          @click.stop
+          :href="`/photos/${item.id}/download`"
+        >
+          <i class="icon ion-md-arrow-round-down"></i>
+        </a>
+      </div>
+
+      <div class="photo__username">
+        {{ item.owner.name }}
+      </div>
+
     </RouterLink>
  
   </div>
