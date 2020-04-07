d227caea7bb8078c3a8262046d1851d60b2aad1e
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-04-08 00:09:43 +0900

    Create PhotoDetailApiTest.php
    写真詳細ページに必要なjsonデータの取得をテスト

---
+++ src/laravel/tests/Feature/PhotoDetailApiTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Photo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PhotoDetailApiTest extends TestCase
+{
+    use RefreshDatabase;
+    /** 
+     * @test
+     */ 
+    public function should__正しい構造のJsonを取得する()
+    {
+        factory(Photo::class)->create();
+        $photo = Photo::first();
+
+        $response = $this->json('GET', route('photo.show', [
+            'id' => $photo->id,
+        ]));
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'id' => $photo->id,
+                'url' => $photo->url,
+                'owner' => [
+                    'name' => $photo->owner->name,
+                ],
+            ]);
+    }
+}
