924444ea741220dcbdcd4c784f63e91ed2ad92bc
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-04-14 04:07:10 +0900

    Update CommentFactory.php
    コメントダミーデータ作成

--- src/laravel/database/factories/CommentFactory.php
+++ src/laravel/database/factories/CommentFactory.php
@@ -2,11 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+// use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Comment::class, function (Faker $faker) {
     return [
-        //
+        'content' => substr($faker->text, 0, 500),
+        'user_id' => fn() => factory(App\User::class)->create()->id,
     ];
 });
