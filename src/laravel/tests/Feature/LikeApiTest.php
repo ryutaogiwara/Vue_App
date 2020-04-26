8eb04d5ecebf49cad69a1dc888f36cc275d52938
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-04-27 00:11:06 +0900

    Create LikeApiTest.php
    いいねはステータス管理のようについていれば１そうでなければ他の数字を代入し、その値をチェックして状態管理を行う

---
+++ src/laravel/tests/Feature/LikeApiTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Photo;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LikeApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+
+        factory(Photo::class)->create();
+        $this->photo = Photo::first;
+    }
+
+    /**
+     * @test
+     */
+    public function should_いいねを追加出来る()
+    {
+        // actingAsは認証
+        $response = $this->actingAs($this->user)
+            ->json('PUT', route('photo.like', [
+                'id' => $this->post->id,
+            ]));
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'photo_id' => $this->photo->id,
+            ]);
+
+        // いいねがついているかどうかは0,1でステータス管理を行う
+        $this->assertEquals(1, $this->photo->likes()->count());
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_2回同じ写真にいいねがついても1個しかいいねがつかない()
+    {
+        $param = ['id' => $this->photo->id];
+        $this->actingAs($this->user)->json('PUT', route('photo.like', $param));
+        // 2回目のいいね処理
+        $this->actingAs($this->user)->json('PUT', route('photo.like', $param));
+)
+        $this->assertEquals(1, $this->photo->likes()->count());
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_いいねを解除できる()
+    {
+        $this->photo->likes()->attach($this->user->id);
+
+        $response = $this->actingAs($this->user)
+            ->json('DELETE', route('photo.like', [
+                'id' => $this->photo->id,
+            ]));
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'photo_id' => $this->photo->id,
+            ]);
+
+        $this->assertEquals(0, $this->photo->likes()->count());
+    }
+}
